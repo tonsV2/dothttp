3c70e75deb1bc952bc6b03e38ebd109785292864
sj <sebastianthegreatful@gmail.com>
2019-07-27 21:46:22 +0200

    Application can be invoked from the command line by: python dothttp.py

--- dothttp.py
+++ dothttp.py
@@ -11,35 +11,43 @@ import click
 @click.argument('files', nargs=-1, type=click.File('r'), required=True)
 def cli(files):
     for file in files:
-        # Parse http request
-        request_line, headers_and_body = file.read().split('\n', 1)
-        message = email.message_from_file(StringIO(headers_and_body))
+        request(file)
 
-        method, request_uri = request_line.split(' ')
-        headers = dict(message.items())
-        message_body = message.get_payload()
 
-        # Perform http request
-        parsed = urlparse(request_uri)
-        hostname = parsed.netloc
-        path = parsed.path
-        if parsed.query:
-            path = '%s?%s' % (path, parsed.query)
-        connection = {}
-        if parsed.scheme == 'http':
-            connection = HTTPConnection(hostname)
-        elif parsed.scheme == 'https':
-            connection = HTTPSConnection(hostname, timeout=8, context=ssl._create_unverified_context())
-        headers['Host'] = parsed.netloc
-        connection.request(method, path, message_body, headers=headers)
-        response = connection.getresponse()
-        body = response.read().decode('utf8')
-        response.close()
-        connection.close()
+def request(file):
+    # Parse http request
+    request_line, headers_and_body = file.read().split('\n', 1)
+    message = email.message_from_file(StringIO(headers_and_body))
 
-        # Print http response
-        print("%s %s" % (response.status, response.reason))
-        response_headers = response.headers
-        for key in response_headers:
-            print("%s: %s" % (key, response_headers[key]))
-        pprint(body)
+    method, request_uri = request_line.split(' ')
+    headers = dict(message.items())
+    message_body = message.get_payload()
+
+    # Perform http request
+    parsed = urlparse(request_uri)
+    hostname = parsed.netloc
+    path = parsed.path
+    if parsed.query:
+        path = '%s?%s' % (path, parsed.query)
+    connection = {}
+    if parsed.scheme == 'http':
+        connection = HTTPConnection(hostname)
+    elif parsed.scheme == 'https':
+        connection = HTTPSConnection(hostname, timeout=8, context=ssl._create_unverified_context())
+    headers['Host'] = parsed.netloc
+    connection.request(method, path, message_body, headers=headers)
+    response = connection.getresponse()
+    body = response.read().decode('utf8')
+    response.close()
+    connection.close()
+
+    # Print http response
+    print("%s %s" % (response.status, response.reason))
+    response_headers = response.headers
+    for key in response_headers:
+        print("%s: %s" % (key, response_headers[key]))
+    pprint(body)
+
+
+if __name__ == '__main__':
+    cli()
